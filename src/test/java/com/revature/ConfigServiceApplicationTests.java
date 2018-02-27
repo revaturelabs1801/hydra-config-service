445b060cd108195eae6a3e4df96f4fa142f2e926
Anthony Libardi <libardi42@gmail.com>
2018-02-27 11:33:33 -0500

    Okay so I formatted the file by accident

--- src/test/java/com/revature/ConfigServiceApplicationTests.java
+++ src/test/java/com/revature/ConfigServiceApplicationTests.java
@@ -7,11 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { ConfigServiceApplicationTests.class })
-public class ConfigServiceApplicationTests
-{
-    
-    @Test
-    public void contextLoads()
-    {}
-    
+public class ConfigServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
 }
