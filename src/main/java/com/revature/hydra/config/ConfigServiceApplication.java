7255d227764fc6f68181aaed33c61ec7498f647f
Hasanain <hasanain.abdullah94@gmail.com>
2018-02-28 11:26:05 -0500

    renamed the main package to com.revature.hydra.config

--- src/main/java/com/revature/hydra/config/ConfigServiceApplication.java
+++ src/main/java/com/revature/hydra/config/ConfigServiceApplication.java
@@ -1,4 +1,4 @@
-package com.revature.hydra;
+package com.revature.hydra.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
